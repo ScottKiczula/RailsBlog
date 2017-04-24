b6603a5e113158b0e24c6cd2036a361f66944c95
ScottKiczula <kiczulascott@gmail.com>
2017-04-23 20:59:39 -0500

    Add helper methods for current user and require user. Add controller methods for sessions. Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
 
   get 'signup' => 'users#new'
+
   get 'login' => 'sessions#new'
-  
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
