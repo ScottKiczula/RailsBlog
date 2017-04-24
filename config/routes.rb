fe74bf0d3d637aa82fb4c5bd28a5331bb5cd322b
ScottKiczula <kiczulascott@gmail.com>
2017-04-23 20:31:00 -0500

    Clean up routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
+  resources :users
+
   resources :articles do
     resources :comments
   end
 
   get 'signup' => 'users#new'
-
   get 'login' => 'sessions#new'
-
-  resources :users
   
-  root 'welcome#index'
-
 end
